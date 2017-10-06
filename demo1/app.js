75e1f08b4153c5386299ef329e35f77806f5806f
Daniel <a13761839322@163.com>
2017-10-06 19:26:36 +0800

    rxjs demo

--- demo1/app.js
+++ demo1/app.js
@@ -1,5 +1,19 @@
 import Rx from 'rxjs/Rx';
-Rx.Observable
-	.fromEvent(document.body, 'click') // 註冊監聽
-    .take(1)
-	.subscribe(console.log);
+// Rx.Observable
+// 	.fromEvent(document.body, 'click') // 註冊監聽
+//     .take(1)
+// 	.subscribe(console.log);
+
+
+
+
+var observable = Rx.Observable
+	.create(function(observer) {
+		observer.next('Jerry'); // RxJS 4.x 以前的版本用 onNext
+		observer.next('Anna');
+	})
+
+// 訂閱這個 observable
+observable.subscribe(function(value) {
+	console.log(value);
+})
---
+++ demo2/app.js
@@ -0,0 +1,17 @@
+import Rx from 'rxjs/Rx';
+
+const dragDOM = document.getElementById('drag');
+const body = document.body;
+
+const mouseDown = Rx.Observable.fromEvent(dragDOM, 'mousedown');
+const mouseUp = Rx.Observable.fromEvent(body, 'mouseup');
+const mouseMove = Rx.Observable.fromEvent(body, 'mousemove');
+
+mouseDown
+  .map(event => mouseMove.takeUntil(mouseUp))
+  .concatAll()
+  .map(event => ({ x: event.clientX, y: event.clientY }))
+  .subscribe(pos => {
+  	dragDOM.style.left = pos.x + 'px';
+    dragDOM.style.top = pos.y + 'px';
+  })
